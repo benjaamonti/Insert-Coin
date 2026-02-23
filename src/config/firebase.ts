7825502c59f767d7170613b9df8209998f661530
benjaamonti <150205790+benjaamonti@users.noreply.github.com>
2026-02-23 18:50:43 -0300

    Update Firebase configuration

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDummyKeyForGameHub123456789",
-  authDomain: "gamehub-multijugador.firebaseapp.com",
-  databaseURL: "https://gamehub-multijugador-default-rtdb.firebaseio.com",
-  projectId: "gamehub-multijugador",
-  storageBucket: "gamehub-multijugador.firebasestorage.app",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890"
+  apiKey: "AIzaSyBiZV7fityZooHwomVjtfMSq5vX31o9OeM",
+  authDomain: "insert-coin-github.firebaseapp.com",
+  databaseURL: "https://insert-coin-github-default-rtdb.firebaseio.com",
+  projectId: "insert-coin-github",
+  storageBucket: "insert-coin-github.firebasestorage.app",
+  messagingSenderId: "898080637289",
+  appId: "1:898080637289:web:32f32b267849fe41c7fdf8"
 };
 
 const app = initializeApp(firebaseConfig);
